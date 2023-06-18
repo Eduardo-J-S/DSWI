e6a18a87528e931281ee5a25dfc3409135e37a64
Eduardo J.S <103137675+Eduardo-J-S@users.noreply.github.com>
2023-06-17 22:05:41 -0300

    Renderização do json na tela home

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -34,5 +34,36 @@ window.addEventListener('resize', function(event) {
 fetch('livros.json')
 .then(e=>e.json())
 .then(e=>{
-    let
+    function add_card(obj){
+        let div = document.createElement("div")
+    // let obj = e[4]
+    let title = obj.titulo
+    let img = obj.imgCapa
+    let data = obj.ano
+    let overview = obj.descricao
+    let estoq = obj.estoque
+
+    let estrutura = `<section>
+         <div class="card_image">
+             <img src="${img}" alt="">
+         </div>
+         <div class="card_body">
+             <h4>${title}</h4>
+             <div class="card_status">
+                 <p class="badge">${overview}</p>
+                 <p class="badge">
+                     <img width="10" src="" alt="">
+                     ${estoq}
+                 </p>
+                 <p>${data}</p>
+             </div>
+         </div>
+     </section>
+     `
+
+     div.innerHTML = estrutura
+     cards.appendChild(div)
+    }
+
+    e.map(add_card)
 })
