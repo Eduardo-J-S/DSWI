a673d665a10a6edc310b8a7763eeb10658122a7e
Eduardo J.S <103137675+Eduardo-J-S@users.noreply.github.com>
2023-06-19 00:44:04 -0300

    Adicionando modal para os livros e modificando o css dos livros e do modal

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -31,38 +31,81 @@ window.addEventListener('resize', function(event) {
     }
 })
 
+
+const modal = document.querySelector("dialog")
+
 fetch('livros.json')
 .then(e=>e.json())
 .then(e=>{
+
+    function showModal(obj) {
+        let modalContent = document.createElement("div");
+        modalContent.classList.add("modal-content");
+      
+        let title = obj.titulo;
+        let img = obj.imgCapa;
+        let data = obj.ano;
+        let overview = obj.descricao;
+        let estoq = obj.estoque;
+      
+        let modalStructure = `
+          <div class="modal-header">
+            <h2>${title}</h2>
+            <span class="close">&times;</span>
+          </div>
+          <div class="modal-body">
+            <img src="${img}" alt="">
+            <p>${overview}</p>
+            <p>Estoque: ${estoq}</p>
+            <p>Ano: ${data}</p>
+          </div>
+        `;
+      
+        modalContent.innerHTML = modalStructure;
+      
+        let modal = document.getElementById("modal");
+        modal.style.display = "block";
+        modal.innerHTML = ""; // Limpa o conteúdo anterior
+        modal.appendChild(modalContent);
+      
+        // Fecha o modal quando o usuário clica no botão de fechar
+        let closeButton = modalContent.querySelector(".close");
+        closeButton.addEventListener("click", () => {
+            modal.style.display = "none";
+            modal.innerHTML = ""; // Limpa o conteúdo do modal ao fechar
+        });
+    }
+      
     function add_card(obj){
+        
         let div = document.createElement("div")
-    // let obj = e[4]
-    let title = obj.titulo
-    let img = obj.imgCapa
-    let data = obj.ano
-    let overview = obj.descricao
-    let estoq = obj.estoque
+        let title = obj.titulo
+        let img = obj.imgCapa
+        let data = obj.ano
+        let estoq = obj.estoque
 
-    let estrutura = `<section>
-         <div class="card_image">
-             <img src="${img}" alt="">
-         </div>
-         <div class="card_body">
-             <h4>${title}</h4>
-             <div class="card_status">
-                 <p class="badge">${overview}</p>
-                 <p class="badge">
-                     <img width="10" src="" alt="">
-                     ${estoq}
-                 </p>
-                 <p>${data}</p>
-             </div>
-         </div>
-     </section>
-     `
+        let estrutura = `<section class="card">
+            <div class="card_image">
+                <img src="${img}" alt="">
+            </div>
+            <div>
+                <h4>${title}</h4>
+                <div class="card_status">
+                    <p>${estoq}</p>
+                    <p>${data}</p>
+                </div>
+            </div>
+        </section>
+        `
 
-     div.innerHTML = estrutura
-     cards.appendChild(div)
+        div.innerHTML = estrutura
+
+
+         div.addEventListener("click", ()=>{
+            showModal(obj)
+        })
+
+        cards.appendChild(div)
     }
 
     e.map(add_card)
