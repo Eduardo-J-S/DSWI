d4359aa50e67fbf9066b2fc2b3bbbf2f108bb6a7
Eduardo J.S <103137675+Eduardo-J-S@users.noreply.github.com>
2023-06-20 02:04:17 -0300

    criação do footer e iniciando responsividade no body

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -1,39 +1,3 @@
-var header = document.getElementById('header');
-var navigationHeader = document.getElementById('navigation_header');
-var content = document.getElementById('content');
-var showSidebar = false;
-
-
-function toggleSidebar(){
-    showSidebar = !showSidebar;
-    if(showSidebar){
-        navigationHeader.style.marginLeft = '-10vw';
-        navigationHeader.style.animationName = 'showSidebar';
-        content.style.filter = 'blur(2px)';
-    } else{
-        navigationHeader.style.marginLeft = '-100vw';
-        navigationHeader.style.animationName = '';
-        content.style.filter = '';
-    }
-}
-
-function closeSidebar(){
-    if(showSidebar){
-        showSidebar = true;
-        toggleSidebar();
-    }
-}
-
-window.addEventListener('resize', function(event) {
-    if(window.innerWidth > 768 && showSidebar) {
-        showSidebar = true;
-        toggleSidebar();
-    }
-})
-
-
-const modal = document.querySelector("dialog")
-
 fetch('livros.json')
 .then(e=>e.json())
 .then(e=>{
